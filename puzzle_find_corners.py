f59e6623ecadb888011b36114483f1e94b3d002d
Peter-M-S <peter.m.schrader@gmail.com>
2022-09-18 02:46:21 +0200

    before img_bw from list to array

--- puzzle_find_corners.py
+++ puzzle_find_corners.py
@@ -31,7 +31,7 @@ def draw_corners_on_img(img, corners, rims):
 
     plt.imshow(img, cmap="Greys_r")
     plt.show()
-    # return img
+    return img
 
 
 def plot_to_pdf(images_list):
@@ -43,7 +43,7 @@ def plot_to_pdf(images_list):
 
     for i, img in enumerate(images_list):
         plt.subplot(plots, 2, i + 1)
-        plt.imshow(img, cmap="binary")
+        plt.imshow(img, cmap="Greys_r")
 
     plt.savefig("puzzle_image_analysis.pdf")
     # plt.show()
@@ -57,7 +57,7 @@ def find_my_corners(img_bw):
 
     # clip img to no full white lines
     top = 0
-    bottom = rows
+    bottom = rows-1
     for i, r in enumerate(img_bw):
         if any(c == 0 for c in r):
             top = i
@@ -82,10 +82,10 @@ def find_my_corners(img_bw):
 
     # add half_frame to each side
     frame_cols, frame_rows = int(rows/5), int(cols/5)
-    top -= round(frame_rows/2)
-    bottom += round(frame_rows/2)
-    left -= int(frame_cols/2)
-    right += int(frame_cols/2)
+    top = max(0, top - round(frame_rows/2))
+    bottom = min(rows-1, bottom + round(frame_rows/2))
+    left = max(0, left - round(frame_cols/2))
+    right = min(cols-1, cols + round(frame_cols/2))
     print(top, bottom)
     print(left, right)
     red_img_bw = []
@@ -100,6 +100,7 @@ def find_my_corners(img_bw):
     # plt.imshow(red_img_bw, cmap="Greys_r")
     # plt.show()
 
+    # initialize frame
     top, left = 0, 0
     step = 100
     frame = Frame((top, left), (frame_rows, frame_cols))
@@ -124,54 +125,7 @@ def find_my_corners(img_bw):
         # move frame to next position
         frame = frame.move_frame_in_array(red_img_bw, step)
 
-    print(corners)
-    print(rims)
-    img_np = np.array(red_img_bw, dtype=np.uint8)
-    print(img_np.shape)
-    draw_corners_on_img(img_np, corners, rims)
-    exit()
-
-    half_a = 50
-    pixels_tot = (half_a * 2) ** 2
-    pixels_min = pixels_tot / 4 * 0.8
-    pixels_max = pixels_tot / 4 * 1.2
-    for row in range(half_a, rows - 2 * half_a, half_a):
-        for col in range(half_a, cols - 2 * half_a, half_a):
-            if img[row][col]:
-                continue
-            print(row, col)
-            black = set()
-            is_black_quarter = False
-            for x in range(row - half_a, row + 2 * half_a, 1):
-                if len(black) > pixels_max:
-                    break
-                for y in range(col - half_a, col + 2 * half_a, 1):
-                    if len(black) > pixels_max:
-                        break
-                    if not img[x][y]:
-                        black.add((x, y))
-            if pixels_min < len(black):
-                is_black_quarter = True
-
-            if is_black_quarter:
-                cluster = 0
-                for x, y in black:
-                    white_neighbours = 0
-                    for xx in range(x - 1, x + 2, 1):
-                        if white_neighbours:
-                            break
-                        for yy in range(y - 1, y + 2, 1):
-                            if white_neighbours:
-                                break
-                            if (xx, yy) not in black:
-                                white_neighbours += 1
-                    if white_neighbours == 9:
-                        cluster += 1
-
-                if cluster >= (len(black) - (2 * half_a)) * 0.9:
-                    corners.append(np.array([row, col]))
-
-    return np.array(corners)
+    return red_img_bw, corners, rims
 
 
 if __name__ == '__main__':
@@ -184,39 +138,24 @@ if __name__ == '__main__':
     for f in file_type_list(PATH, "jpg"):
         print(f"file: {f}")
 
-        # img = cv2.imread(PATH + f)
-        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #
-        # gray = np.float32(gray)
-        # dst = cv2.cornerHarris(gray, 2, 3, 0.1)
-        # # result is dilated for marking the corners, not important
-        # dst = cv2.dilate(dst, None)
-        # # Threshold for an optimal value, it may vary depending on the image.
-        # img[dst > 0.01 * dst.max()] = [0, 255, 0]
-        # cv2.imshow('dst', img)
-        # if cv2.waitKey(0) & 0xff == 27:
-        #     cv2.destroyAllWindows()
-
         THRESHOLD = 25
         img_gray = cv2.imread(PATH + f, cv2.IMREAD_GRAYSCALE)
         img_bw = img_gray
         img_bw[img_gray <= THRESHOLD] = 0
         img_bw[img_gray > THRESHOLD] = 255
 
-        list_bw = img_bw.tolist()
+        # list_bw = img_bw.tolist()
         # plt.imshow(list_bw, cmap="Greys_r")
         # plt.show()
-        corners = find_my_corners(list_bw)
-        print(corners)
-        exit()
-        # corners = cv2.goodFeaturesToTrack(img, 16, 0.00001, w * 0.2)
-        # corners = np.int0(corners)
-        draw_corners_on_img(img, corners)
+        red_img_bw, corners, rims = find_my_corners(img_bw)
+        img_np = np.array(red_img_bw, dtype=np.uint8)
+        # print(img_np.shape)
+        img = draw_corners_on_img(img_np, corners, rims)
         # assert len(corners) == len(SIDES)
         # continue
         # img_bw = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         # keep img_bw clean for analysing
         images.append(img)
 
-    # plot_to_pdf(images)
+    plot_to_pdf(images)
     # assert len(corners) == len(SIDES)
